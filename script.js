9536f9530898b264ef93dce0bc12e928c98be627
Təvəkkül <tevizeynalov@gmail.com>
2022-04-16 02:12:22 +0400

    feat

--- script.js
+++ script.js
@@ -1,23 +1,60 @@
-'use strict';
+'use strict'
 
-const DomElement = function (selector, height, width, bg, fontSize) {
+const DomElement = function (selector, height, width, bg, fontSize, position) {
     this.selector = selector;
     this.height = height;
     this.width = width;
     this.bg = bg;
     this.fontSize = fontSize;
+    this.position = position;
 
     this.newMethod = function () {
-        let div = document.createElement('div');
-        div.innerHTML = `${this.selector} ${this.height} ${this.width} ${this.bg} ${this.fontSize}`;
-        div.innerText = 'БЛОК!';
-        div.height = '100px';
-        div.width = '100px';
-        div.backgroundColor = bg = 'red';
-        div.fontSize = '12px';
+        let newElem;
+        if (this.selector[0] === '.') {
+            newElem = document.createElement('div');
+            newElem.classList.add(this.selector.slice(1));
+        } else if (this.selector[0] === '#') {
+            newElem = document.createElement('p');
+            newElem.id = this.selector.slice(1);
+        } else {
+            newElem = document.createElement(this.selector);
+        }
+
+        newElem.style.cssText = `height: ${this.height}px; width: ${this.width}px; background: ${this.bg}; font-size: ${this.fontSize}px; position: ${this.position};`;
+
+        document.body.append(newElem);
     };
 };
 
-const kv = new DomElement('div', '100px', '100px', 'red', '12px');
-kv.newMethod();
-console.log(kv);
+const square = new DomElement('.blue', 100, 100, 'red', 14, 'absolute');
+
+document.addEventListener('DOMContentLoaded', () => {
+    square.newMethod();
+});
+
+
+document.addEventListener('keydown', (event) => {
+    const elem = document.querySelector('.blue');
+
+    let top = +elem.style.top.slice(0, elem.style.top.length - 2) || 0;
+    let left = +elem.style.left.slice(0, elem.style.left.length - 2) || 0;
+
+    switch (event.code) {
+        case "ArrowUp":
+            top -= 10;
+            elem.style.top = top + 'px';
+            break;
+        case "ArrowDown":
+            top += 10;
+            elem.style.top = top + 'px';
+            break;
+        case "ArrowLeft":
+            left -= 10;
+            elem.style.left = left + 'px';
+            break;
+        case "ArrowRight":
+            left += 10;
+            elem.style.left = left + 'px';
+            break;
+    }
+});
